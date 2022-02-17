467c43572bb9d7d3de54a044fe212468157cdce3
Ronan Amicel <ronan.amicel@gmail.com>
2022-02-17 22:07:02 +0100

    Désactive le mode debug de Django

--- parrainage/project/settings.py
+++ parrainage/project/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get("SECRET_KEY", "$bi$*vapnotv4gh^#t)bzegc#^z_9po-o)*j)^4o*#hk&@um$+")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 try:
