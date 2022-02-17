65fe27a1bc306dde8c49e1d19c3c34b3f8f18116
Ronan Amicel <ronan.amicel@gmail.com>
2022-02-17 22:06:35 +0100

    Permet de configurer la clé secrète via l’environnement

--- parrainage/project/settings.py
+++ parrainage/project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$bi$*vapnotv4gh^#t)bzegc#^z_9po-o)*j)^4o*#hk&@um$+'
+SECRET_KEY = os.environ.get("SECRET_KEY", "$bi$*vapnotv4gh^#t)bzegc#^z_9po-o)*j)^4o*#hk&@um$+")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
